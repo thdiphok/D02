57b8c16f6f356f142d0049a2a51475d41bf1f58c
Thapelo Diphoko <thdiphok@c4r20s3.wethinkcode.co.za>
2020-07-09 12:57:16 +0200

    first commit

---
+++ ex03/ft_is_negative.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+void ft_is_negative(int n)
+{	
+	if (n < 0)
+		{
+			ft_putchar('N');
+	}
+	else
+{	
+		ft_putchar('P');
+}
+	ft_putchar('\n');
+}
+int main(void)
+{
+	ft_is_negative(-1);
+	ft_is_negative(0);
+	return 0;
+}
