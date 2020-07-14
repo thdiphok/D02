8b0bdbbdbda4fe489897aba85bd3f0a2210760da
Thapelo Diphoko <thdiphok@c4r20s3.wethinkcode.co.za>
2020-07-14 08:05:03 +0200

    modified

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thdiphok <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/14 08:04:04 by thdiphok          #+#    #+#             */
+/*   Updated: 2020/07/14 08:04:43 by thdiphok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(char c)
@@ -18,7 +30,7 @@ void ft_is_negative(int n)
 }
 int main(void)
 {
-	ft_is_negative(-1);
-	ft_is_negative(0);
+	ft_is_negative();
+	
 	return 0;
 }
